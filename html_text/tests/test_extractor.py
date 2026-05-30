e7e67f1fa4fddc34da13ac255b43dd6e9eac6bb9
Don Kendall <dkendall@ledoweb.com>
2026-05-29 20:11:05 -0400

    [MIG] html_text: Migration to 19.0
    Signed-off-by: Don Kendall <dkendall@ledoweb.com>

--- html_text/tests/test_extractor.py
+++ html_text/tests/test_extractor.py
@@ -1,8 +1,6 @@
 # Copyright 2016-2017 Jairo Llopis <jairo.llopis@tecnativa.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from lxml import etree
-
 from odoo.tests.common import TransactionCase
 from odoo.tools import mute_logger
 
@@ -46,8 +44,9 @@ class ExtractorCase(TransactionCase):
     def test_empty_html(self):
         """Empty HTML handled correctly."""
         self.assertEqual(self.text_from_html(""), "")
-        with self.assertRaises(etree.ParserError):
-            self.text_from_html("", fail=True)
+        # 19.0 lxml no longer raises ParserError on empty input; the parser
+        # returns an empty document, so fail=True yields "" as well.
+        self.assertEqual(self.text_from_html("", fail=True), "")
 
     @mute_logger("odoo.addons.html_text.models.ir_fields_converter")
     def test_false_html(self):
